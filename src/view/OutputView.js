268fcaf3ddfbada9afb6856765fa12ba1c6530a4
ddaeunbb <ddaeunbb@gmail.com>
2023-11-06 14:17:58 +0900

    [Feat]: 로또 구매 개수 출력 - 로또를 구매한 총 개수를 출력하는 기능을 구현하였습니다.

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,5 +1,15 @@
-class OutputView {
+import { Console } from '@woowacourse/mission-utils';
+import { OUTPUT_MESSAGE } from '../constants/message';
 
+class OutputView {
+  printLineBreak(){
+    Console.print('\n');
+  }
+
+  printTotalLotto(num){
+    this.printLineBreak();
+    Console.print(`${num}${OUTPUT_MESSAGE.boughtLottoCount}`)
+  }  
 }
 
 export default OutputView;
